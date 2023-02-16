b545ba0e72315e5b64f93041763d53c32fae36f1
woohyeonjoe <qlqkthd8802@gmail.com>
2023-02-16 19:28:46 +0900

    스프링 이벤트

--- src/main/java/com/example/springevent/SpringEventApplication.java
+++ src/main/java/com/example/springevent/SpringEventApplication.java
@@ -2,8 +2,10 @@ package com.example.springevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringEventApplication {
 
     public static void main(String[] args) {
